0f39d247dc888e6bab7ea66af51020389c474522
killreal321 <kr.sushilnikov@ukr.net>
2020-11-17 19:21:38 +0200

    AK2_Lab3

---
+++ CMakeFiles/AK2_Lab3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AK2_Lab3"
+  "AK2_Lab3.pdb"
+  "CMakeFiles/AK2_Lab3.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AK2_Lab3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,119 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+    int flags, opt;
+    int nsecs, tfnd;
+
+   nsecs = 0;
+    tfnd = 0;
+    flags = 0;
+    while ((opt = getopt(argc, argv, "nt:")) != -1) {
+        switch (opt) {
+        case 'n':
+            flags = 1;
+            break;
+        case 't':
+            nsecs = atoi(optarg);
+            tfnd = 1;
+            break;
+        default: /* '?' */
+            fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n",
+                    argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+   printf("flags=%d; tfnd=%d; optind=%d\n", flags, tfnd, optind);
+
+   if (optind >= argc) {
+        fprintf(stderr, "Expected argument after options\n");
+        exit(EXIT_FAILURE);
+    }
+
+   printf("name argument = %s\n", argv[optind]);
+
+
+   exit(EXIT_SUCCESS);
+}
+
+#include <stdio.h>    
+#include <stdlib.h>   
+#include <getopt.h>
+
+int
+main(int argc, char **argv)
+{
+    int c;
+    int digit_optind = 0;
+
+   while (1) {
+        int this_option_optind = optind ? optind : 1;
+        int option_index = 0;
+        static struct option long_options[] = {
+            {"help",     required_argument, 0,  0 },
+            {"version",  no_argument,       0,  0 },
+            {"delete",  required_argument, 0,  0 },
+            {"create",  required_argument, 0, 'c'},
+            {"list",    required_argument, 0,  0 },
+            {0,         0,                 0,  0 }
+        };
+
+       c = getopt_long(argc, argv, "abc:d:012",
+                 long_options, &option_index);
+        if (c == -1)
+            break;
+
+       switch (c) {
+        case 0:
+            printf("option %s", long_options[option_index].name);
+            if (optarg)
+                printf(" with arg %s", optarg);
+            printf("\n");
+            break;
+
+       case '0':
+        case '1':
+        case '2':
+            if (digit_optind != 0 && digit_optind != this_option_optind)
+              printf("digits occur in two different argv-elements.\n");
+            digit_optind = this_option_optind;
+            printf("option %c\n", c);
+            break;
+
+       case 'a':
+            printf("option a\n");
+            break;
+
+       case 'b':
+            printf("option b\n");
+            break;
+
+       case 'c':
+            printf("option c with value '%s'\n", optarg);
+            break;
+
+       case 'd':
+            printf("option d with value '%s'\n", optarg);
+            break;
+
+       case '?':
+            break;
+
+       default:
+            printf("?? getopt returned character code 0%o ??\n", c);
+        }
+    }
+
+   if (optind < argc) {
+        printf("non-option ARGV-elements: ");
+        while (optind < argc)
+            printf("%s ", argv[optind++]);
+        printf("\n");
+    }
+
+   exit(EXIT_SUCCESS);
+}
